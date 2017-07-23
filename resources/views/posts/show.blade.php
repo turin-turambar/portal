00c406bc9e1ce9cb6e71765c426d95af29300e66
Nemanja <nemanja.djokic@gmail.com>
2017-07-23 18:08:28 +0200

    Comments for blog posts added. Some modifications to bllog layout

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,5 +7,33 @@
 		<p>
 			{{$post->blogPost}}
 		</p>
+		<hr>
+		<div class="card">
+			<div class="card-block">
+				<form action="/posts/{{$post->id}}/comments" method="POST">
+					{{csrf_field()}}
+					<div class="form-group">
+						<textarea name="body" class="form-control" rows="3" placeholder="Ostavite komentar ovde" required></textarea>
+					</div>
+					<div class="form-group">
+						<button class="btn btn-primary">Objavi komentar</button>
+					</div>
+				</form>
+				@include('layouts.errors')
+			</div>
+		</div>
+		<h3>Svi komentari:</h3>
+		<div class="comments card">
+			<div class="card-block">
+				@foreach($post->comments as $comment)
+					<article>
+						<strong class="boldComment">
+							{{$comment->created_at->diffForHumans()}}: &nbsp;
+						</strong>
+						{{$comment->body}}
+					</article>
+				@endforeach
+			</div>
+		</div>
 	</div>
 @endsection
